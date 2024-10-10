b0f7f6861cca0b9998cba16d45c3a5a62ab65d5c
jjw05130513022@gmail.com <jjw05130513022@gmail.com>
2024-10-10 23:08:50 +0900

    fix

--- cap_model/TransDreamer/engine/train_dreamer_transformer.py
+++ cap_model/TransDreamer/engine/train_dreamer_transformer.py
@@ -64,12 +64,11 @@ def simulate_test(model, test_env, cfg, global_step, device):
       obs = next_obs
 
 def train(model, cfg, device):
-  wandb.init(project='3ball_CAP', entity='hails', name='TransD_20x20_1e7',config={
+  wandb.init(project='3ball_CAP', entity='hails', name='TransD_20x20_500',config={
     "batch_size": cfg.train.batch_size,
-    "model_lr" : cfg.optimize.model_lr,
-    "value_lr" : cfg.optimize.value_lr,
-    "actor_lr" : cfg.optimize.actor_lr,
-    "rl_discount":cfg.rl.discount,
+    # "overshooting_distance": cfg.overshooting_distance,
+
+    # "planning_discount": cfg.discount,
     "total_episodes": cfg.total_steps,
     "max_steps": cfg.env.max_steps,
   })
@@ -118,9 +117,6 @@ def train(model, cfg, device):
   train_env.reset()
   steps = count_steps(datadir, cfg)
   length = 0
-  print("total_steps :", cfg.total_steps)
-  print("max_steps :", cfg.env.max_steps)
-  print("roomsize is : 20")
   print("Collecting prefill-experience...")
   while steps < cfg.arch.prefill: # prefill 이라고 되어있는 것에서 알수 있듯이 그냥 채우는거(지정한 step 수를 넘기면 breakk)
     action = train_env.sample_random_action() # 진짜 말 그대로 random action 을 뽑겠다는 뜻
@@ -180,6 +176,11 @@ def train(model, cfg, device):
           "steps": steps_in_episode,
           "reward": reward
         }, step=episode_num)
+
+        # 로그 출력
+        print("episode:", episode_num)
+        print("reward:", reward)
+        print("steps_in_episode:", steps_in_episode)
 
         # 에피소드 카운트 증가 및 환경 초기화
         episode_num += 1
@@ -241,7 +242,15 @@ def train(model, cfg, device):
         for k, v in grad_norm.items():
           writer.add_scalar('train_grad_norm/' + k, v, global_step=global_step)
 
-
+    # wandb.log({
+    #   "episode": episode_num,
+    #   "steps": global_step,
+    #   "reward": reward,
+    #   # "model_loss": model_loss.item(),  # 모델 손실 기록
+    #   #"actor_loss": actor_loss.item(),  # Actor 손실 기록
+    #   #"value_loss": value_loss.item(),  # Value 손실 기록
+    #   # "mean_value": np.mean(episode_values),  # 필요시 평균 값 기록
+    # }, step=episode_num)
     # evaluate RL
     if global_step % cfg.train.eval_every_step == 0:
       simulate_test(model, test_env, cfg, global_step, device)
@@ -254,3 +263,193 @@ def train(model, cfg, device):
 
   writer.close()
   wandb.close()
+# def train(model, cfg, device):
+#     wandb.init(project='3ball_CAP', entity='hails', config={
+#         "batch_size": cfg.train.batch_size,
+#         #"overshooting_distance": cfg.overshooting_distance,
+#
+#
+#
+#         #"planning_discount": cfg.discount,
+#         "total_episodes" : cfg.total_steps,
+#         "max_steps": cfg.env.max_steps,
+#     })
+#     print("======== Settings ========")
+#     pprint(cfg)
+#     print("======== Model ========")
+#     pprint(model)
+#
+#     model = model.to(device)
+#
+#     # Optimizer 및 체크포인터 설정
+#     optimizers = get_optimizer(cfg, model)
+#     checkpointer_path = os.path.join(cfg.checkpoint.checkpoint_dir, cfg.exp_name, cfg.env.name, cfg.run_id)
+#     checkpointer = Checkpointer(checkpointer_path, max_num=cfg.checkpoint.max_num)
+#
+#     # 설정 파일 저장
+#     with open(checkpointer_path + '/config.yaml', 'w') as f:
+#       cfg.dump(stream=f, default_flow_style=False)
+#       print(f"config file saved to {checkpointer_path + '/config.yaml'}")
+#
+#     # 체크포인트 로드 (있을 경우)
+#     if cfg.resume:
+#       checkpoint = checkpointer.load(cfg.resume_ckpt)
+#       if checkpoint:
+#         model.load_state_dict(checkpoint['model'])
+#         for k, v in optimizers.items():
+#           if v is not None:
+#             v.load_state_dict(checkpoint[k])
+#         env_step = checkpoint['env_step']
+#         global_step = checkpoint['global_step']
+#       else:
+#         env_step = 0
+#         global_step = 0
+#     else:
+#       env_step = 0
+#       global_step = 0
+#
+#     # TensorBoard 설정
+#     writer = SummaryWriter(log_dir=os.path.join(cfg.logdir, cfg.exp_name, cfg.env.name, cfg.run_id), flush_secs=30)
+#
+#     # 환경 설정 및 초기화
+#     datadir = os.path.join(cfg.data.datadir, cfg.exp_name, cfg.env.name, cfg.run_id, 'train_episodes')
+#     test_datadir = os.path.join(cfg.data.datadir, cfg.exp_name, cfg.env.name, cfg.run_id, 'test_episodes')
+#     train_env = make_env(cfg, writer, 'train', datadir, store=True)
+#     test_env = make_env(cfg, writer, 'test', test_datadir, store=True)
+#
+#     # 초기 프리필 단계에서 랜덤 행동으로 데이터 수집
+#     train_env.reset()
+#     steps = count_steps(datadir, cfg)
+#     length = 0
+#     while steps < cfg.arch.prefill:
+#       action = train_env.sample_random_action()
+#       next_obs, reward, done = train_env.step(action[0])
+#       length += 1
+#       steps += done * length
+#       length = length * (1. - done)
+#       if done:
+#         train_env.reset()
+#
+#     steps = count_steps(datadir, cfg)
+#     print(f'collected {steps} steps. Start training...')
+#     train_ds = EnvIterDataset(datadir, cfg.train.train_steps, cfg.train.batch_length)
+#     train_dl = DataLoader(train_ds, batch_size=cfg.train.batch_size, num_workers=4)
+#     train_iter = iter(train_dl)
+#     global_step = max(global_step, steps)
+#
+#     obs = train_env.reset()
+#     state = None
+#     action_list = torch.zeros(1, 1, cfg.env.action_size).float()  # T, C
+#     action_list[0, 0, 0] = 1.0
+#     input_type = cfg.arch.world_model.input_type
+#     temp = cfg.arch.world_model.temp_start
+#     episode_num = 0  # 에피소드 카운터
+#
+#     train_steps = 0
+#     total_train_steps = 0
+#     # 에피소드 단위 학습 루프
+#     while episode_num < cfg.total_steps:
+#       with torch.no_grad():
+#         model.eval()
+#         total_reward = 0  # 현재 에피소드의 총 보상
+#         done = False
+#         print("episode is now :", episode_num)
+#         while not done:
+#           train_steps = 0
+#           # 환경과 상호작용
+#           next_obs, reward, done = train_env.step(action_list[0, -1].detach().cpu().numpy())
+#           train_steps += 1
+#           prev_image = torch.tensor(obs[input_type])
+#           next_image = torch.tensor(next_obs[input_type])
+#
+#           # 모델의 정책에 따라 행동 선택
+#           action_list, state = model.policy(
+#             prev_image.to(device), next_image.to(device), action_list.to(device),
+#             global_step, 0.1, state, context_len=cfg.train.batch_length
+#           )
+#           total_train_steps += train_steps
+#           total_reward += reward
+#           obs = next_obs
+#           if done:
+#             print("total_train_steps per episode : ", train_steps)
+#             train_env.reset()
+#             state = None
+#             action_list = torch.zeros(1, 1, cfg.env.action_size).float()
+#             action_list[0, 0, 0] = 1.0
+#
+#       # 에피소드 종료 후 학습 진행
+#       model.train()
+#       traj = next(train_iter)
+#       for k, v in traj.items():
+#         traj[k] = v.to(device).float()
+#
+#       logs = {}
+#
+#       # 모델 및 옵티마이저 업데이트
+#       model_optimizer = optimizers['model_optimizer']
+#       model_optimizer.zero_grad()
+#       transformer_optimizer = optimizers['transformer_optimizer']
+#       if transformer_optimizer is not None:
+#         transformer_optimizer.zero_grad()
+#
+#       model_loss, model_logs, prior_state, post_state = model.world_model_loss(global_step, traj, temp)
+#       grad_norm_model = model.world_model.optimize_world_model(model_loss, model_optimizer, transformer_optimizer,
+#                                                                writer, global_step)
+#
+#       if cfg.arch.world_model.transformer.warm_up:
+#         lr = anneal_learning_rate(global_step, cfg)
+#         for param_group in transformer_optimizer.param_groups:
+#           param_group['lr'] = lr
+#       else:
+#         lr = cfg.optimize.model_lr
+#
+#       actor_optimizer = optimizers['actor_optimizer']
+#       value_optimizer = optimizers['value_optimizer']
+#       actor_optimizer.zero_grad()
+#       value_optimizer.zero_grad()
+#
+#       # Actor와 Value의 손실 계산 및 최적화
+#       actor_loss, value_loss, actor_value_logs = model.actor_and_value_loss(global_step, post_state, traj, temp)
+#       grad_norm_actor = model.optimize_actor(actor_loss, actor_optimizer, writer, global_step)
+#       grad_norm_value = model.optimize_value(value_loss, value_optimizer, writer, global_step)
+#
+#       wandb.log({
+#           "episode": episode_num,
+#           "steps": train_steps,
+#           "reward": total_reward,
+#           "model_loss": model_loss.item(),  # 모델 손실 기록
+#           "actor_loss": actor_loss.item(),  # Actor 손실 기록
+#           "value_loss": value_loss.item(),  # Value 손실 기록
+#           # "mean_value": np.mean(episode_values),  # 필요시 평균 값 기록
+#       }, step=episode_num)
+#
+#       # 로그 기록
+#       if global_step % cfg.train.log_every_step == 0:
+#         logs.update(model_logs)
+#         logs.update(actor_value_logs)
+#         model.write_logs(logs, traj, global_step, writer)
+#         writer.add_scalar('train_hp/lr', lr, global_step)
+#
+#         grad_norm = dict(
+#           grad_norm_model=grad_norm_model,
+#           grad_norm_actor=grad_norm_actor,
+#           grad_norm_value=grad_norm_value,
+#         )
+#
+#         for k, v in grad_norm.items():
+#           writer.add_scalar('train_grad_norm/' + k, v, global_step=global_step)
+#
+#       # 주기적인 평가 및 체크포인트 저장
+#       if global_step % cfg.train.eval_every_step == 0:
+#         simulate_test(model, test_env, cfg, global_step, device)
+#
+#       if global_step % cfg.train.checkpoint_every_step == 0:
+#         env_step = count_steps(datadir, cfg)
+#         checkpointer.save('', model, optimizers, global_step, env_step)
+#
+#       # 에피소드 수 증가
+#       episode_num += 1
+#       global_step += 1
+#
+#     writer.close()
+#     wandb.close()
--- cap_model/TransDreamer/envs/minigrid_wrapper.py
+++ cap_model/TransDreamer/envs/minigrid_wrapper.py
@@ -94,7 +94,333 @@ class GymGridEnv():
     self.action_repeat = action_repeat
     self._step_counter = 0
     self._random = np.random.RandomState(seed=None)
+    self.life_done = life_dimport atexit
+import threading
+import gymnasium as gym
+import gym
+import gym_minigrid
+#from gym_minigrid.wrappers import *
+from minigrid.wrappers import *
+import cv2
+import torch
+from torch import distributions as torchd
+import numpy as np
+import pdb
+from custom_env.GymMoreRedBalls import GymMoreRedBalls
+from gymnasium.core import Wrapper
+
+
+
+
+class ActionSpaceWrapper(Wrapper):
+  def __init__(self, env: gym.Env, max_steps, new_action_space: int):
+    super().__init__(env)
+    self.new_action_space = new_action_space
+    self.action_space = spaces.Discrete(new_action_space)
+    self.env.action_space = self.action_space
+
+    self.max_steps = max_steps
+    self.env.max_steps = max_steps
+
+  def reset(self, **kwargs):
+    # Reset the environment and update the max_steps
+    state = super().reset()
+
+    self.env.max_steps = self.max_steps
+    return state
+
+
+class MaxStepsWrapper(Wrapper):
+  def __init__(self, env: gym.Env, max_steps: int,action_repeat,
+               new_action_space=3):
+    super().__init__(env)
+    self.max_steps = max_steps
+    self.env.max_steps = max_steps
+    self.env.env.max_steps = max_steps
+
+
+    # self._env.seed(seed)
+
+    self.action_repeat = action_repeat
+
+
+    self.action_space = spaces.Discrete(new_action_space)
+    self.env.action_space = self.action_space
+    self.env.env.action_space = self.action_space
+    # self.env.env.evm.action_space = self.action_space
+
+  def reset(self, **kwargs):
+    state = super().reset()
+
+    self.env.max_steps = self.max_steps
+    return state
+
+  def step(self, action):
+    observation, r, terminated, truncated, info = self.env.step(action)
+
+    return observation, r, terminated, truncated, info
+
+
+
+class FullyCustom(FullyObsWrapper):
+  def __init__(self, env: gym.Env, max_steps: int):
+    super().__init__(env)
+    self.max_steps = max_steps
+    self.env.max_steps = max_steps
+
+  def reset(self, **kwargs):
+    # Reset the environment and update the max_steps
+    state = super().reset()
+
+    self.env.max_steps = self.max_steps
+    return state
+
+class GymGridEnv():
+  LOCK = threading.Lock()
+
+  def __init__(self, name, action_repeat, max_steps, life_done=False):
+    super().__init__()
+    with self.LOCK:
+      env = GymMoreRedBalls(room_size=10,render_mode ="rgb_array")
+      #env = RGBImgPartialObsWrapper(env, tile_size=9)  # Get pixel observations, (63, 63, 3)
+      env = ActionSpaceWrapper(env, max_steps,new_action_space=3)
+      env = FullyCustom(env, max_steps)  # Get rid of the 'mission' field
+      self._env = MaxStepsWrapper(env, max_steps, action_repeat, new_action_space=3)
+      self._env.max_steps = max_steps #self._env.env.env.env.env.max_steps = 5000
+    self.action_repeat = action_repeat
+    self._step_counter = 0
+    self._random = np.random.RandomState(seed=None)
     self.life_done = life_done
+    self.max_steps = max_steps
+    #self.action_size = 6
+    self.action_size = 3
+
+  def reset(self):
+
+    self._step_counter = 0  # Reset internal timer
+    with self.LOCK:
+      observation = self._env.reset()
+
+    if isinstance(observation, tuple):
+      observation = observation[0]['image']  # 첫 번째 값이 이미지일 가능성이 큼
+
+    #  observation = self._env.render(mode='rgb_array')
+    observation = cv2.resize(observation, (64, 64), interpolation=cv2.INTER_LINEAR)
+    observation = np.clip(observation, 0, 255).astype(np.uint8)
+    observation = np.transpose(observation, (2, 0, 1)) # 3, 64, 64
+    self._step_counter = 0
+
+    return {'image': observation}
+
+  def step(self, action):
+    reward = 0
+    RESET = False
+    for k in range(self.action_repeat):
+      observation, reward_k, done, _,info = self._env.step(action)
+      reward += reward_k
+      self._step_counter += 1  # Increment internal timer
+
+      if done:
+        observation = self._env.reset()
+        RESET = True
+
+      if self.life_done:
+        done = self._step_counter == self.max_steps
+
+      if RESET:
+        break
+
+    if isinstance(observation, dict) :
+      observation = observation['image']
+
+    elif isinstance(observation, tuple) :
+      observation = observation[0]['image']
+
+    # observation = self._env.render(mode='rgb_array')
+    observation = cv2.resize(observation, (64, 64), interpolation=cv2.INTER_LINEAR)
+    observation = np.clip(observation, 0, 255).astype(np.uint8)
+    observation = np.transpose(observation, (2, 0, 1)) # 3, 64, 64
+
+    return {'image': observation}, reward, done, info
+
+  def render(self):
+    self._env.render()
+
+  def close(self):
+    self._env.close()
+
+  @property
+  def observation_space(self):
+    shape = (3, 64, 64)
+    space = gym.spaces.Box(low=0, high=255, shape=shape, dtype=np.uint8)
+    return gym.spaces.Dict({'image': space})
+
+  @property
+  def action_space(self):
+    return gym.spaces.Discrete(self.action_size)
+
+class OneHotAction():
+  def __init__(self, env):
+    self._env = env
+    pass
+  def __getattr__(self, name):
+    return getattr(self._env, name)
+
+  def step(self, action):
+    index = np.argmax(action).astype(int)
+    reference = np.zeros_like(action)
+    reference[index] = 1
+    if not np.allclose(reference, action):
+      raise ValueError(f'Invalid one-hot action:\n{action}')
+    return self._env.step(index)
+
+  def reset(self):
+    return self._env.reset()
+
+  def sample_random_action(self):
+    action = np.zeros((1, self._env.action_space.n,), dtype=float)
+    idx = np.random.randint(0, self._env.action_space.n, size=(1,))[0]
+    action[0, idx] = 1
+    return action
+
+class TimeLimit():
+  def __init__(self, env, duration, time_penalty):
+    self._env = env
+    self._step = None
+    self._duration = duration
+    self.time_penalty = time_penalty
+
+  def __getattr__(self, name):
+    return getattr(self._env, name)
+
+  def step(self, action):
+    assert self._step is not None, 'Must reset environment.'
+    obs, reward, done, info = self._env.step(action)
+    self._step += 1
+    if self.time_penalty:
+      reward = reward - 1. / self._duration
+
+    if self._step >= self._duration:
+      done = True
+      if 'discount' not in info:
+        info['discount'] = np.array(1.0).astype(np.float32)
+      self._step = None
+    return obs, reward, done, info
+
+  def reset(self):
+    self._step = 0
+    return self._env.reset()
+
+class Collect:
+
+  def __init__(self, env, callbacks=None, precision=32):
+    self._env = env
+    self._callbacks = callbacks or ()
+    self._precision = precision
+    self._episode = None
+
+  def __getattr__(self, name):
+    return getattr(self._env, name)
+
+  def step(self, action):
+    #action = action[0]
+    obs, reward, done, info = self._env.step(action)
+    obs = {k: self._convert(v) for k, v in obs.items()}
+    transition = obs.copy()
+    transition['action'] = action
+    transition['reward'] = reward
+    transition['discount'] = info.get('discount', np.array(1 - float(done)))
+    transition['done'] = float(done)
+    self._episode.append(transition)
+    if done:
+      episode = {k: [t[k] for t in self._episode] for k in self._episode[0]}
+      episode = {k: self._convert(v) for k, v in episode.items()}
+      info['episode'] = episode
+      for callback in self._callbacks:
+        callback(episode)
+    obs['image'] = obs['image'][None,...]
+    return obs, reward, done, info
+
+  def reset(self):
+    obs = self._env.reset()
+    transition = obs.copy()
+    transition['action'] = np.zeros(self._env.action_space.n)
+    transition['reward'] = 0.0
+    transition['discount'] = 1.0
+    transition['done'] = 0.0
+    self._episode = [transition]
+    obs['image'] = obs['image'][None,...]
+    return obs
+
+  def _convert(self, value):
+    value = np.array(value)
+    if np.issubdtype(value.dtype, np.floating):
+      dtype = {16: np.float16, 32: np.float32, 64: np.float64}[self._precision]
+    elif np.issubdtype(value.dtype, np.signedinteger):
+      dtype = {16: np.int16, 32: np.int32, 64: np.int64}[self._precision]
+    elif np.issubdtype(value.dtype, np.uint8):
+      dtype = np.uint8
+    else:
+      pdb.set_trace()
+      raise NotImplementedError(value.dtype)
+    return value.astype(dtype)
+
+class RewardObs:
+
+  def __init__(self, env):
+    self._env = env
+    pass
+  def __getattr__(self, name):
+    return getattr(self._env, name)
+
+  @property
+  def observation_space(self):
+    spaces = self._env.observation_space.spaces
+    assert 'reward' not in spaces
+    spaces['reward'] = gym.spaces.Box(-np.inf, np.inf, dtype=np.float32)
+    return gym.spaces.Dict(spaces)
+
+  def step(self, action):
+
+    obs, reward, done, info = self._env.step(action)
+    obs['reward'] = reward
+    return obs, reward, done
+
+  def reset(self):
+    obs = self._env.reset()
+    obs['reward'] = 0.0
+    return obs
+
+class NormalizeActions:
+
+  def __init__(self, env):
+    self._env = env
+    self._mask = np.logical_and(
+      np.isfinite(env.action_space.low),
+      np.isfinite(env.action_space.high))
+    self._low = np.where(self._mask, env.action_space.low, -1)
+    self._high = np.where(self._mask, env.action_space.high, 1)
+
+    self.random_actor = torchd.independent.Independent(
+      torchd.uniform.Uniform(torch.Tensor(env.action_space.low)[None],
+                             torch.Tensor(env.action_space.high)[None]), 1)
+
+  def __getattr__(self, name):
+    return getattr(self._env, name)
+
+  @property
+  def action_space(self):
+    low = np.where(self._mask, -np.ones_like(self._low), self._low)
+    high = np.where(self._mask, np.ones_like(self._low), self._high)
+    return gym.spaces.Box(low, high, dtype=np.float32)
+
+  def step(self, action):
+    original = (action + 1) / 2 * (self._high - self._low) + self._low
+    original = np.where(self._mask, original, action)
+    return self._env.step(original)
+
+  def sample_random_action(self):
+    return self.random_actor.sample()[0].numpy()one
     self.max_steps = max_steps
     #self.action_size = 6
     self.action_size = 3
